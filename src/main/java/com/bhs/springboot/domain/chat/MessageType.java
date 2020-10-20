2294c3fd0df24fbc2ca0a730b216d5414f4761af
morba9850 <morba9850@naver.com>
2020-10-20 11:02:42 +0900

    채팅방

---
+++ src/main/java/com/bhs/springboot/domain/chat/MessageType.java
@@ -0,0 +1,7 @@
+package com.bhs.springboot.domain.chat;
+
+public enum MessageType {
+    CHAT,
+    JOIN,
+    LEAVE
+}
---
+++ src/main/java/com/bhs/springboot/web/ChatController.java
@@ -0,0 +1,32 @@
+package com.bhs.springboot.web;
+
+import com.bhs.springboot.domain.chat.ChatMessage;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ChatController {
+
+    @MessageMapping("/chat.sendMessage")
+    @SendTo("/topic/public")
+    public ChatMessage sendMessage(@Payload ChatMessage chatMessage) {
+        return chatMessage;
+    }
+
+    @MessageMapping("/chat.addUser")
+    @SendTo("/topic/public")
+    public ChatMessage addUser(@Payload ChatMessage chatMessage, SimpMessageHeaderAccessor headerAccessor){
+        headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
+        return chatMessage;
+    }
+
+    @GetMapping("/index2")
+    public String index2() {
+        return "index2";
+    }
+
+}
